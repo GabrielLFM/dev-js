27674ec50050990748ab4c443a979d1e7dfe7b23
GabrielLFM <biel.lucianomelo@gmail.com>
2026-03-04 06:02:26 -0300

    feat: Lab activities completed

---
+++ aulas/three/app.js
@@ -0,0 +1,10 @@
+let idade = 17;
+const nome = "Gabriel Luciano";
+let cidade = "São paulo";
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos e moro em ${cidade}`);
+
+let num1 = 15;
+let num2 = 95;
+let soma = num1 + num2;
+console.log(`o resultado da soma é ${soma}` );
---
+++ aulas/two/tipo.js
@@ -0,0 +1,19 @@
+let nome = "sete";
+let numero = 7;
+let validade = 1;
+
+console.log(typeof nome);
+console.log(typeof numero);
+console.log(typeof validade);
+
+nome = 7;
+numero ="nove";
+validade = true
+
+console.log(typeof nome);
+console.log(typeof numero);
+console.log(typeof validade);
+
+// A variável nome mudou de string para number.
+// A variável numero mudou de number para string.
+// A variável validade mudou de number para boolean.
