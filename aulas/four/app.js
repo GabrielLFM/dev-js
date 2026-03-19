3abca9a8af4e3027698e401d7964cd60b6f32f07
GabrielLFM <biel.lucianomelo@gmail.com>
2026-03-19 08:42:27 -0300

    feat:Lab 4 activity completed.

---
+++ aulas/four/app.js
@@ -0,0 +1,30 @@
+let mes = 4; 
+
+switch (mes) {
+  case 1: console.log("Janeiro"); break;
+  case 2: console.log("Fevereiro"); break;
+  case 3: console.log("Março"); break;
+  case 4: console.log("Abril"); break;
+  case 5: console.log("Maio"); break;
+  case 6: console.log("Junho"); break;
+  case 7: console.log("Julho"); break;
+  case 8: console.log("Agosto"); break;
+  case 9: console.log("Setembro"); break;
+  case 10: console.log("Outubro"); break;
+  case 11: console.log("Novembro"); break;
+  case 12: console.log("Dezembro"); break;
+  default: console.log("Mês inválido");
+}
+
+
+let nota = 8;
+
+if (nota >= 9) {
+  console.log("Excelente");
+} else if (nota >= 7) {
+  console.log("Bom");
+} else if (nota >= 5) {
+  console.log("Regular");
+} else {
+  console.log("Reprovado");
+}
